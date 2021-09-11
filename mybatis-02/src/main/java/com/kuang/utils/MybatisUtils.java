539d7b93ae794cd2fe11295934bb6719d0260b81
amzpiper <1131717341@qq.com>
2021-09-11 19:09:54 +0800

    配置文件

---
+++ mybatis-02/src/main/java/com/kuang/utils/MybatisUtils.java
@@ -0,0 +1,36 @@
+package com.kuang.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * sqlSessionFactory
+ * @author 郭宇航
+ */
+public class MybatisUtils {
+
+    //提升作用域-局部变全局
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            // 使用mybatis第一步
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = null;
+            inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSession getSqlSession() {
+        return sqlSessionFactory.openSession();
+    }
+
+}
--- mybatis-02/src/test/java/com/kuang/dao/UserMapperTest.java
+++ mybatis-02/src/test/java/com/kuang/dao/UserMapperTest.java
@@ -5,40 +5,18 @@ import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 public class UserMapperTest {
 
     @Test
-    public void test() {
+    public void test1() {
         //第一步：获得sqlSession对象
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         //第二步：面向接口编程,获取UserDao,执行SQL
         //方式1
         List<User> userList = sqlSession.getMapper(UserMapper.class).getUserList();
-        for (User user : userList) {
-            System.out.println(user.toString());
-        }
-        //方式2不推荐
-        userList = sqlSession.selectList("com.kuang.com.kuang.com.kuang.dao.UserDao.getUserList");
-        for (User user : userList) {
-            System.out.println(user.toString());
-        }
-
-        //关闭sqlSession
-        sqlSession.close();
-    }
-
-    @Test
-    public void test1() {
-        //第一步：获得sqlSession对象
-        SqlSession sqlSession = MybatisUtils.getSqlSession();
-
-        //第二步：面向接口编程,获取UserDao,执行SQL
-        List<User> userList = sqlSession.getMapper(UserMapper.class).getUserListLike("roo");
         for (User user : userList) {
             System.out.println(user.toString());
         }
@@ -64,26 +42,6 @@ public class UserMapperTest {
     }
 
     /**
-     * 测试查询通过id
-     */
-    @Test
-    public void test21() {
-        //第一步：获得sqlSession对象
-        SqlSession sqlSession = MybatisUtils.getSqlSession();
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("id", 4);
-        map.put("name", "root");
-
-        //第二步：面向接口编程,获取UserDao,执行SQL
-        User user = sqlSession.getMapper(UserMapper.class).getUserById2(map);
-        System.out.println(user.toString());
-
-        //关闭sqlSession
-        sqlSession.close();
-    }
-
-    /**
      * 插入
      */
     @Test
@@ -94,32 +52,6 @@ public class UserMapperTest {
         //第二步：面向接口编程,获取UserDao,执行SQL
         User user = new User(4,"root","1234");
         int res = sqlSession.getMapper(UserMapper.class).addUser(user);
-        if (res>0){
-            System.out.println("插入成功");
-        }
-
-        //提交事务
-        sqlSession.commit();
-        //关闭sqlSession
-        sqlSession.close();
-    }
-
-    /**
-     * 插入by map
-     * 好处：很多附带属性为空也能用
-     */
-    @Test
-    public void test31() {
-        //第一步：获得sqlSession对象
-        SqlSession sqlSession = MybatisUtils.getSqlSession();
-
-        //第二步：面向接口编程,获取UserDao,执行SQL
-        Map<String, Object> userMap = new HashMap<String, Object>();
-        userMap.put("userId", 5);
-        userMap.put("userName", "root");
-        userMap.put("userPaw", "root");
-
-        int res = sqlSession.getMapper(UserMapper.class).addUser2(userMap);
         if (res>0){
             System.out.println("插入成功");
         }
