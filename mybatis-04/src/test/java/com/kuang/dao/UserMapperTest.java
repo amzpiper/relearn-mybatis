3b9b7866b2a65a3440d6140507850567c2378165
amzpiper <1131717341@qq.com>
2021-09-12 23:00:42 +0800

    Log4J日志

--- mybatis-04/src/test/java/com/kuang/dao/UserMapperTest.java
+++ mybatis-04/src/test/java/com/kuang/dao/UserMapperTest.java
@@ -3,6 +3,7 @@ package com.kuang.dao;
 import com.kuang.pojo.User;
 import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.util.List;
@@ -25,81 +26,11 @@ public class UserMapperTest {
         sqlSession.close();
     }
 
-    /**
-     * 测试查询通过id
-     */
+    static Logger logger = Logger.getLogger(UserMapperTest.class);
     @Test
-    public void test2() {
-        //第一步：获得sqlSession对象
-        SqlSession sqlSession = MybatisUtils.getSqlSession();
-
-        //第二步：面向接口编程,获取UserDao,执行SQL
-        User user = sqlSession.getMapper(UserMapper.class).getUserById(1);
-        System.out.println(user.toString());
-
-        //关闭sqlSession
-        sqlSession.close();
-    }
-
-    /**
-     * 插入
-     */
-    @Test
-    public void test3() {
-        //第一步：获得sqlSession对象
-        SqlSession sqlSession = MybatisUtils.getSqlSession();
-
-        //第二步：面向接口编程,获取UserDao,执行SQL
-        User user = new User(4,"root","1234");
-        int res = sqlSession.getMapper(UserMapper.class).addUser(user);
-        if (res>0){
-            System.out.println("插入成功");
-        }
-
-        //提交事务
-        sqlSession.commit();
-        //关闭sqlSession
-        sqlSession.close();
-    }
-
-    /**
-     * 修改
-     */
-    @Test
-    public void test4() {
-        //第一步：获得sqlSession对象
-        SqlSession sqlSession = MybatisUtils.getSqlSession();
-
-        //第二步：面向接口编程,获取UserDao,执行SQL
-        User user = new User(4,"root","root");
-        int res = sqlSession.getMapper(UserMapper.class).updateUser(user);
-        if (res>0){
-            System.out.println("修改成功");
-        }
-
-        //提交事务
-        sqlSession.commit();
-        //关闭sqlSession
-        sqlSession.close();
-    }
-
-    /**
-     * 删除
-     */
-    @Test
-    public void test5() {
-        //第一步：获得sqlSession对象
-        SqlSession sqlSession = MybatisUtils.getSqlSession();
-
-        //第二步：面向接口编程,获取UserDao,执行SQL
-        int res = sqlSession.getMapper(UserMapper.class).deleteUser(4);
-        if (res>0){
-            System.out.println("删除成功");
-        }
-
-        //提交事务
-        sqlSession.commit();
-        //关闭sqlSession
-        sqlSession.close();
+    public void testLog4j() {
+        logger.info("info:进入testLog4j方法");
+        logger.debug("debug:进入调试语句");
+        logger.error("error:trycatch紧急");
     }
 }
