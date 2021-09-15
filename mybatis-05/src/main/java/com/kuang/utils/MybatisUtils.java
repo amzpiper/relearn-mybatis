acb41cd767bd74646131591df5427937ee24710a
amzpiper <1131717341@qq.com>
2021-09-15 21:42:58 +0800

    自动提交事务

--- mybatis-05/src/main/java/com/kuang/utils/MybatisUtils.java
+++ mybatis-05/src/main/java/com/kuang/utils/MybatisUtils.java
@@ -29,6 +29,10 @@ public class MybatisUtils {
         }
     }
 
+    /**
+     * //自动提交事务
+     * @return SqlSession
+     */
     public static SqlSession getSqlSession() {
         return sqlSessionFactory.openSession(true);
     }
